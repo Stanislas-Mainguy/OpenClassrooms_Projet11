be453d1c9a88de926420b23ba479d50a01319094
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-11-30 17:25:25 +0100

    Ajout logique de routage

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ErrorBoundary from "./HomePageErrorBoundaries";
+import Header from "../../components/Organisms/Header/Header";
+import Main from "../../components/Organisms/Main/Main";
+import Footer from "../../components/Organisms/Footer/Footer";
+
+const HomePage = () => {
+    return (
+        <ErrorBoundary>
+            <Header />
+            <Routes>
+                <Route exact path="/" element={<Main />} />
+                <Route path="/signin" element={<Main signIn={true} />} />
+            </Routes>
+            <Footer />
+        </ErrorBoundary>
+    );
+};
+
+export default HomePage;
