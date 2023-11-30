5c4116b0d63fe5ee5de68bb9cd4946774ab0e16c
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-11-30 17:18:20 +0100

    Mise en place link pour le routage

--- src/components/Atoms/Logo/Logo.jsx
+++ src/components/Atoms/Logo/Logo.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Logo.css";
 
 const Logo = () => {
     return (
-        <a className="main-nav-logo" href="#">
+        <Link to="/" className="main-nav-logo">
             <img
                 className="main-nav-logo-image"
                 src={process.env.PUBLIC_URL + "/img/argentBankLogo.png"}
                 alt="Argent Bank Logo"
             />
             <h1 className="sr-only">Argent Bank</h1>
-        </a>
+        </Link>
     );
 };
 
