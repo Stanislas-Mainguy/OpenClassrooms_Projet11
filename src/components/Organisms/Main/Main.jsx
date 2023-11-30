509c21456fb48792e3b50dfeea2ae4bdb6c9e70a
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-11-30 17:22:04 +0100

    Création changement du main suivant routage

--- src/components/Organisms/Main/Main.jsx
+++ src/components/Organisms/Main/Main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import SectionSignIn from "../../Molecules/SectionSignIn/SectionSignIn";
 import SectionHero from "../../Molecules/SectionHero/SectionHero";
 import SectionFeatures from "../../Molecules/SectionFeatures/SectionFeatures";
 import "./Main.css";
 
-const Main = () => {
+const Main = ({ signIn }) => {
+    if (signIn) {
+        return (
+            <main className="main bg-dark">
+                <SectionSignIn />
+            </main>
+        );
+    };
+    
     return (
         <main>
             <SectionHero />
