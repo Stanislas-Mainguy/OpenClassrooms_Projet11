afd8ed9da5926d27f08d75f8527fe258d7a73f89
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-01 16:59:01 +0100

    Ajout logique pour redux

--- src/components/Organisms/Main/Main.jsx
+++ src/components/Organisms/Main/Main.jsx
@@ -1,24 +1,38 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import SectionSignIn from "../../Molecules/SectionSignIn/SectionSignIn";
 import SectionHero from "../../Molecules/SectionHero/SectionHero";
 import SectionFeatures from "../../Molecules/SectionFeatures/SectionFeatures";
+import SectionAccount from "../../Molecules/SectionAccount/SectionAccount";
 import "./Main.css";
 
-const Main = ({ signIn }) => {
-    if (signIn) {
+const Main = () => {
+    const isAuthenticated = useSelector(state => state.isAuthenticated);
+    const location = useLocation();
+
+    const onSignInPage = location.pathname === '/signin';
+
+    if (isAuthenticated) {
+        return (
+            <main className="main bg-dark">
+                <SectionAccount />
+            </main>
+        );
+    } else if (onSignInPage) {
         return (
             <main className="main bg-dark">
                 <SectionSignIn />
             </main>
         );
-    };
-    
-    return (
-        <main>
-            <SectionHero />
-            <SectionFeatures />
-        </main>
-    );
+    } else {
+        return (
+            <main>
+                <SectionHero />
+                <SectionFeatures />
+            </main>
+        );
+    }
 };
 
 export default Main;
