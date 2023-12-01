168d32b353eb47e05c8ac942d30cc9749c11c6a7
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-01 16:57:21 +0100

    Changement titre

---
+++ src/components/Atoms/HeroContent/HeroContent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HeroContent.css";
+
+const SectionHeroContent = () => {
+    return (
+        <section className="hero-content">
+          <h2 className="sr-only">Promoted Content</h2>
+          <p className="subtitle">No fees.</p>
+          <p className="subtitle">No minimum deposit.</p>
+          <p className="subtitle">High interest rates.</p>
+          <p className="text">Open a savings account with Argent Bank today!</p>
+        </section>
+    );
+};
+
+export default SectionHeroContent;
