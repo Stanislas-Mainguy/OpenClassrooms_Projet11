51693a15353768a176b10f33d4ea818801288476
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-01 16:57:34 +0100

    Ajout logique pour redux

--- src/components/Atoms/SignIn/SignIn.jsx
+++ src/components/Atoms/SignIn/SignIn.jsx
@@ -1,16 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import "./SignIn.css";
 
 const SignIn = () => {
-    return (
-        <div>
-            <Link to="/signin" className="main-nav-item">
-                <i className="fa fa-user-circle"></i>
-                Sign In
-            </Link>
-        </div>
-    );
+    const isAuthenticated = useSelector(state => state.isAuthenticated);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSignOut = () => {
+        dispatch({ type: 'SIGN_OUT' });
+        navigate("/");
+    };
+
+    if (isAuthenticated) {
+        return (
+            <div>
+                <Link to="/profile" className="main-nav-item">
+                    <i className="fa fa-user-circle"></i>
+                    Tony
+                </Link>
+                <button onClick={handleSignOut} className="main-nav-item">
+                    <i className="fa fa-sign-out"></i>
+                    Sign Out
+                </button>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <Link to="/signin" className="main-nav-item">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </Link>
+            </div>
+        );
+    }
 };
 
 export default SignIn;
