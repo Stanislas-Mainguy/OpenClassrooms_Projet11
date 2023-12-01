35414d2ce5008616363bba70594f018d6bd17517
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-01 16:57:45 +0100

    Ajout pour logique redux

--- src/components/Atoms/SignInForm/SignInForm.jsx
+++ src/components/Atoms/SignInForm/SignInForm.jsx
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import "./SignInForm.css";
 
 const SignInForm = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (username === 'Stan' && password === 'Stanislas') {
+            dispatch({ type: 'SIGN_IN'});
+            navigate("/profile");
+        } else {
+            alert("Invalid username or password");
+        }
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="input-wrapper">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" autoComplete="username" />
+                <input 
+                    type="text" 
+                    id="username" 
+                    autoComplete="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)} 
+                />
             </div>
             <div className="input-wrapper">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" autoComplete="current-password" />
+                <input
+                    type="password" 
+                    id="password" 
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                />
             </div>
             <div className="input-remember">
                 <label htmlFor="remember-me">Remember me</label>
                 <input type="checkbox" id="remember-me" />
             </div>
-            <button className="sign-in-button">Sign In</button>
+            <button type="submit" className="sign-in-button">Sign In</button>
         </form>
     );
 };
