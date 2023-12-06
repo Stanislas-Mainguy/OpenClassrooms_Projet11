081313121bfb4d77ec8094bb2a5ecb7e2ace377e
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-06 18:57:35 +0100

    Changement pour Redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,10 @@
-import { configureStore} from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
 
-const initialState = {
-    isAuthenticated: false,
-    isSignInPage: false,
-};
-
-function authReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'SIGN_IN':
-            return { ...state, isAuthenticated: true };
-        case 'SIGN_OUT':
-            return { ...state, isAuthenticated: false };
-        default:
-            return state;
-    };
-};
-
-const store = configureStore({
-    reducer: authReducer,
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
 });
 
 export default store;
