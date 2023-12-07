79c526b3ea4557abfe45e61c69d77b1dc9362118
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-07 14:33:55 +0100

    Changement d'état  logo quand l'utilisateur est co

--- src/components/Atoms/Logo/Logo.jsx
+++ src/components/Atoms/Logo/Logo.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Logo.css";
+import { useDispatch, useSelector } from "react-redux";
 
 const Logo = () => {
-    return (
-        <Link to="/" className="main-nav-logo">
-            <img
-                className="main-nav-logo-image"
-                src={process.env.PUBLIC_URL + "/img/argentBankLogo.png"}
-                alt="Argent Bank Logo"
-            />
-            <h1 className="sr-only">Argent Bank</h1>
-        </Link>
-    );
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+    if (isAuthenticated) {
+        return (
+            <div className="main-nav-logo">
+                <img
+                    className="main-nav-logo-image"
+                    src={process.env.PUBLIC_URL + "/img/argentBankLogo.png"}
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </div>
+        );
+    } else {
+        return (
+            <Link to="/" className="main-nav-logo">
+                <img
+                    className="main-nav-logo-image"
+                    src={process.env.PUBLIC_URL + "/img/argentBankLogo.png"}
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </Link>
+        );
+    } 
 };
 
 export default Logo;
