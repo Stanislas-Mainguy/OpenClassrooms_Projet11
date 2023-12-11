7ef15f3f19cc8dd5ae57a0941a384943b682e028
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-11 12:20:17 +0100

    Test d'intégrtion d'appel

--- src/components/Molecules/SectionAccount/SectionAccount.jsx
+++ src/components/Molecules/SectionAccount/SectionAccount.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUserProfile } from "../../../store/slices/authSlice";
+import { fetchUserProfile, toggleEdit } from "../../../store/slices/authSlice";
 import SectionAccountData from "./SectionAccountData.json";
 import AccountItems from "../../Atoms/AccountItems/AccountItems";
+import EditUserName from "../../Atoms/EditUserName/EditUserName";
 import "./SectionAccount.css";
 
 const SectionAccount = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.auth.user);
     const token = useSelector((state) => state.auth.token);
+    const isEditing = useSelector((state) => state.auth.isEditing);
 
     useEffect(() => {
         if (token) {
@@ -16,15 +18,24 @@ const SectionAccount = () => {
         }  
     }, [dispatch, token]);
 
+    const handleEditClick = () => {
+        dispatch(toggleEdit());
+    };
+
     console.log("User data", user);
     const { firstName, userName } = user || {};
-    
+
     return (
         <>
-            <div className="header">
-                <h1>Welcome back<br />{firstName} {userName}!</h1>
-                <button className="edit-button">Edit Name</button>
-            </div>
+            {isEditing ? (
+                <EditUserName />
+            ) : (
+                <div className="header">
+                    <h1>Welcome back<br />{firstName} {userName}!</h1>
+                    <button className="edit-button" onClick={handleEditClick}>Edit Name</button>
+                </div>
+            )}
+
             <h2 className="sr-only">Accounts</h2>
             {SectionAccountData.map((account, index) => (
                 <AccountItems 
