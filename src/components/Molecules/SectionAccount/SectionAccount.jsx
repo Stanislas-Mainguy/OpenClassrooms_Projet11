902fe65b91f66f5270267a416dc108c0e03990ab
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-12 15:31:05 +0100

    Deleted useState

--- src/components/Molecules/SectionAccount/SectionAccount.jsx
+++ src/components/Molecules/SectionAccount/SectionAccount.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUserProfile, toggleEdit } from "../../../store/slices/authSlice";
 import SectionAccountData from "./SectionAccountData.json";
