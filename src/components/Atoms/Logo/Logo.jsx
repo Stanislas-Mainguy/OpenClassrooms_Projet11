f4e337f7356a6005f319f5fb22b29e9f882322ef
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-13 14:30:39 +0100

    Delet hook non utilisé

--- src/components/Atoms/Logo/Logo.jsx
+++ src/components/Atoms/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./Logo.css";
-import { useDispatch, useSelector } from "react-redux";
 
 const Logo = () => {
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
