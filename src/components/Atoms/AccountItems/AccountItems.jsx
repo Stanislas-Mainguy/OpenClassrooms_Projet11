afc361cc1167704d4b044d7aebef56504432bd22
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-18 09:51:52 +0100

    suppression props pour utilisation par Redux

--- src/components/Atoms/AccountItems/AccountItems.jsx
+++ src/components/Atoms/AccountItems/AccountItems.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAccountData } from '../../../store/slices/authSlice';
 import './AccountItems.css';
 
-const AccountItems = ({ title, amount, description }) => {
+const AccountItems = ({ accountId }) => {
+    const accounts = useSelector(selectAccountData);
+    const account = accounts.find(acc => acc.id === accountId);
+
+    if(!account) return null;
+    
     return (
         <section className="account">
             <div className="account-content-wrapper">
-                <h3 className="account-title">{title}</h3>
-                <p className="account-amount">{amount}</p>
-                <p className="account-amount-description">{description}</p>
+                <h3 className="account-title">{`Argent Bank ${account.accountType}`}</h3>
+                <p className="account-amount">{account.balance}</p>
+                <p className="account-amount-description">{account.description}</p>
             </div>
             <div className="account-content-wrapper cta">
                 <button className="transaction-button">View transactions</button>
