06c526e92899cdd729b9bf17e4535c9908704bd9
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-18 16:40:09 +0100

    deleted

--- src/store/rootReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./slices/authUsers/";
-
-const rootReducer = combineReducers({
-    auth: actionSlice, 
-});
-
-export default rootReducer;
--- src/store/slices/randomUsers/index.js
+++
@@ -1,7 +0,0 @@
-import randomUsersReducer from './usersSlice';
-import { loginUser } from './actions/loginUser';
-
-export {
-  randomUsersReducer,
-  loginUser
-};
