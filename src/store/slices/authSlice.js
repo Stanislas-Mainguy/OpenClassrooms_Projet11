fd7497b76f5cc2f1e2272bfc88e36e5b99853535
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-18 09:54:46 +0100

    Ajout adresse data

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from "axios";
+import SectionAccountData from './SectionAccountData.json';
+import SectionFeaturesData from './SectionFeaturesData.json';
 
 export const loginUser = createAsyncThunk(
     'auth/loginUser',
@@ -11,6 +13,7 @@ export const loginUser = createAsyncThunk(
             });
             return response.data;
         } catch (error) {
+            console.error("Error Response: ", error.response);
             return thunkAPI.rejectWithValue(error.response.data);
         }
     }
@@ -21,13 +24,32 @@ export const fetchUserProfile = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const token = thunkAPI.getState().auth.token;
-      const response = await axios.post('http://localhost:3001/api/v1/user/profile', {
+      const response = await axios.post('http://localhost:3001/api/v1/user/profile', {}, {
         headers: {
           Authorization: `Bearer ${token}`
         }
       });
       return response.data.body;
     } catch (error) {
+      console.error("Error Response: ", error.response);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const fetchUserNameEditProfile = createAsyncThunk(
+  'auth/fetchUserNameEditProfile',
+  async (userData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.token;
+      const response = await axios.put('http://localhost:3001/api/v1/user/profile', userData, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return response.data.body;
+    } catch (error) {
+      console.error("Error Response: ", error.response);
       return thunkAPI.rejectWithValue(error.response.data);
     }
   }
@@ -41,12 +63,15 @@ export const authSlice = createSlice({
     token: null,
     user: null,
     isEditing: false,
+    accounts: SectionAccountData,
+    features: SectionFeaturesData,
   },
   reducers: {
     signOut: (state) => {
       state.isAuthenticated = false;
       state.token = null;
       state.user = null;
+      state.isEditing = false;
     },
     toggleEdit: (state) => {
       state.isEditing = !state.isEditing;
@@ -64,12 +89,24 @@ export const authSlice = createSlice({
       })
       .addCase(fetchUserProfile.fulfilled, (state, action) => {
         state.user = action.payload;
+        localStorage.setItem('userInfos', action.payload.body);
       })
       .addCase(fetchUserProfile.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(fetchUserNameEditProfile.fulfilled, (state, action) => {
+        state.user = {...state.user, ...action.payload};
+      })
+      .addCase(fetchUserNameEditProfile.rejected, (state, action) => {
         state.error = action.payload;
       });
   }
 });
 
+export const selectFeatures = state => state.auth.features;
+export const selectAccountData = state => state.auth.accounts;
+export const selectToken = state => state.auth.token;
+export const selectUser = state => state.auth.user;
+export const selectIsEditing = state => state.auth.isEditing;
 export const { signOut, toggleEdit } = authSlice.actions;
 export default authSlice.reducer;
