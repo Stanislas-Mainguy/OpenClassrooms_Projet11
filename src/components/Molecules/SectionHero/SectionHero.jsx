f255404f62009f7a67b4e39b3d39084bcc9fd43c
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-26 15:26:16 +0100

    Ajout media pour query

--- src/components/Molecules/SectionHero/SectionHero.jsx
+++ src/components/Molecules/SectionHero/SectionHero.jsx
@@ -4,9 +4,15 @@ import "./SectionHero.css";
 
 const SectionHero = () => {
     return (
-        <div className="hero">
+        <section className="hero">
+            <picture>
+                <source media="(min-width: 1200px)" srcSet={process.env.PUBLIC_URL + "/img/bank-tree-desktop.jpg"}/>
+                <source media="(min-width: 992px)" srcSet={process.env.PUBLIC_URL + "/img/bank-tree-laptop.jpg"}/>
+                <source media="(min-width: 768px)" srcSet={process.env.PUBLIC_URL + "/img/bank-tree-tablet.jpg"}/>
+                <img src={process.env.PUBLIC_URL + "/img/bank-tree-mobile.jpg"} alt="picture of plante in a pot"/>
+            </picture>
             <SectionHeroContent />
-        </div>
+        </section>
     );
 };
 
