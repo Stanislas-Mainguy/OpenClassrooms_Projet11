3226ea520cf00f086def43628efec645b1f1115c
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-26 15:25:46 +0100

    changement en webp

--- src/components/Atoms/Logo/Logo.jsx
+++ src/components/Atoms/Logo/Logo.jsx
@@ -11,7 +11,7 @@ const Logo = () => {
             <div className="main-nav-logo">
                 <img
                     className="main-nav-logo-image"
-                    src={process.env.PUBLIC_URL + "/img/argentBankLogo.png"}
+                    src={process.env.PUBLIC_URL + "/img/argentBankLogo.webp"}
                     alt="Argent Bank Logo"
                 />
                 <h1 className="sr-only">Argent Bank</h1>
@@ -22,7 +22,7 @@ const Logo = () => {
             <Link to="/" className="main-nav-logo">
                 <img
                     className="main-nav-logo-image"
-                    src={process.env.PUBLIC_URL + "/img/argentBankLogo.png"}
+                    src={process.env.PUBLIC_URL + "/img/argentBankLogo.webp"}
                     alt="Argent Bank Logo"
                 />
                 <h1 className="sr-only">Argent Bank</h1>
