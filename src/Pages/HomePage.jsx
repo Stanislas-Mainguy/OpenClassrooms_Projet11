3f814eb381abb93cda46744e7114885c6fc92141
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-26 13:41:09 +0100

    Changement de navigation pour utilisateur déco

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ErrorBoundary from "./HomePageErrorBoundaries";
 import Header from "../components/Organisms/Header/Header";
 import Main from "../components/Organisms/Main/Main";
@@ -7,13 +8,18 @@ import Footer from "../components/Organisms/Footer/Footer";
 import ErrorPage404 from "../components/Organisms/ErrorPage404/ErrorPage404";
 
 const HomePage = () => {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
     return (
         <ErrorBoundary>
             <Header />
             <Routes>
                 <Route exact path="/" element={<Main />} />
                 <Route path="/login" element={<Main />} />
-                <Route path="/profile" element={<Main isAuthenticated={true} />} />
+                <Route path="/profile" element={
+                    isAuthenticated ? <Main isAuthenticated={true} /> 
+                    : 
+                    <Navigate replace to="/login" /> } /> 
                 <Route path="*" element={<ErrorPage404 />} />
             </Routes>
             <Footer />
