ccae7263a4d129fe6e85ddac939d9163caa1b063
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-27 13:21:45 +0100

    Changement route

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
+import authReducer from './slices/auth/authSlice';
+import featuresReducer from './slices/features/featuresSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    features: featuresReducer,
   },
 });
 
