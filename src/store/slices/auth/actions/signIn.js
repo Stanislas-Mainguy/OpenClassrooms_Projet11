f802198c12240169a0899e846bb4a98753547479
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-12-27 13:23:21 +0100

    Changement adresse

---
+++ src/store/slices/auth/actions/signIn.js
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async ({ email, password }, thunkAPI) => {
+      try {
+          const response = await axios.post('http://localhost:3001/api/v1/user/login', {
+              email,
+              password
+          });
+          return response.data;
+      } catch (error) {
+          console.error("Error Response: ", error.response);
+          return thunkAPI.rejectWithValue(error.response.data);
+      }
+  }
+);
