ef09ba382b86b42e6e71b448b340074d98cda04c
Stanislas Mainguy <stan.mainguy@gmail.com>
2024-01-10 13:30:37 +0100

    deleted props inutile

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./App";
-import { fetchUserProfile, loginUser } from "./store/slices/auth/authSlice";
+import { loginUser } from "./store/slices/auth/authSlice";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
